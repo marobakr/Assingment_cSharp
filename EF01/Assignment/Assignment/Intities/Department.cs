eeac4e5d9abf63e16c4d2179d594d872637d7d6a
marwanabubakr <dev@marobakr>
2024-09-13 10:37:15 +0300

    EF02

--- EF01/Assignment/Assignment/Intities/Department.cs
+++
@@ -1,9 +0,0 @@
-namespace Assignment.Intities;
-
-public class Department
-{
-    public int Id { get; set; }
-    public string name { get; set; }
-    public int ins_id { get; set; }
-    public DateOnly HiringDate { get; set; }
-}
