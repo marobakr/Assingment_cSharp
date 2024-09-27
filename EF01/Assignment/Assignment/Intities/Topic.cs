76f2943f2be605a8a3e28c65b2e8e23222e710a3
marwanabubakr <dev@marobakr>
2024-09-27 21:48:39 +0300

    MVC_07

--- EF01/Assignment/Assignment/Intities/Topic.cs
+++
@@ -1,7 +0,0 @@
-namespace Assignment.Intities;
-
-public class Topic
-{
-    public int Id { get; set; }
-    public string name { get; set; }
-}
--- EF02/Demo/Enitities/Course.cs
+++
@@ -1,9 +0,0 @@
-namespace Session_01.Enitities;
-
-public class Course
-{
-    public int  Id { get; set; }
-    public string Name { get; set; }
-    public int Age { get; set; }
-    public List<Students> Students { get; set; }
-}
